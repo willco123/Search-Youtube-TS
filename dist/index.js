bc369da47a44ae8ac33e082bdc0460cd7d7185bb
willco213 <87068102+willco123@users.noreply.github.com>
2022-08-17 12:52:05 +0100

    configured eslint

--- dist/index.js
+++ dist/index.js
@@ -1,1 +1,7 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+require("dotenv/config");
+const app_1 = require("./app");
+const port = process.env.PORT || 3004;
+console.log(process.env.NODE_ENV);
+app_1.app.listen(port, () => console.log(`Listening on port ${port}`));
---
+++ src/index.ts
@@ -0,0 +1,6 @@
+import "dotenv/config";
+import { app } from "./app";
+
+const port = process.env.PORT || 3004;
+console.log(process.env.NODE_ENV);
+app.listen(port, () => console.log(`Listening on port ${port}`));
